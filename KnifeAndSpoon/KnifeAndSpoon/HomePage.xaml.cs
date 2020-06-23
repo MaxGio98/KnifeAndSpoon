33c7f403d9aabdf5dac04962beb8000ae5756953
Francesco La Forgia <tecfra98@gmail.com>
2020-06-23 18:09:41 +0200

    Fixed logos

--- KnifeAndSpoon/KnifeAndSpoon/HomePage.xaml.cs
+++ KnifeAndSpoon/KnifeAndSpoon/HomePage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using KnifeAndSpoon.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,10 +17,20 @@ namespace KnifeAndSpoon
     {
         Boolean isFabsOpen;
         Task autoCloseFabs;
+        readonly IList<Ricetta> source;
+        public ObservableCollection<Ricetta> Ricette { get; private set; }
         public HomePage()
         {
             isFabsOpen = false;
             InitializeComponent();
+            source = new List<Ricetta>();
+            source.Add(new Ricetta
+            {
+                Titolo = "PASTAH",
+                ImageUrl = "https://firebasestorage.googleapis.com/v0/b/knifeandspoon-3ac35.appspot.com/o/38c4468c-038e-4f4a-9a21-4cf3f915da2c.jpg?alt=media&token=248808c6-a2b0-47f0-bdbd-8ef0a21f9c4b"
+            });
+            Ricette = new ObservableCollection<Ricetta>(source);
+            TheCarousel.ItemsSource = Ricette;
         }
 
         public void openFabs(object sender, EventArgs args)
@@ -27,7 +39,24 @@ namespace KnifeAndSpoon
             {
                 //Visualizza i pulsanti
                 isFabsOpen = !isFabsOpen;
-                //autoCloseFabs = closeFabsWithWait;
+                Device.StartTimer(new TimeSpan(0, 0, 10), () =>
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        isFabsOpen = !isFabsOpen;
+                        mainFab.RotateTo(-45, 150);
+                        settingsFab.TranslateTo(0, 0, 150);
+                        settingsFab.FadeTo(0, 150); ;
+                        favouriteFab.TranslateTo(0, 0, 150);
+                        favouriteFab.FadeTo(0, 150);
+                        searchFab.TranslateTo(0, 0, 150);
+                        searchFab.FadeTo(0, 150);
+                        addFab.TranslateTo(0, 0, 150);
+                        addFab.FadeTo(0, 150);
+                    });
+                    return false;
+                });
+                
                 mainFab.RotateTo(45, 150);
                 settingsFab.TranslateTo(-210, 0, 150);
                 settingsFab.FadeTo(255, 150);
@@ -67,6 +96,11 @@ namespace KnifeAndSpoon
             searchFab.FadeTo(0, 150);
             addFab.TranslateTo(0, 0, 150);
             addFab.FadeTo(0, 150);
+        }
+
+        public void openRicetta(object sender, EventArgs args)
+        {
+            Console.WriteLine(TheCarousel.Position);
         }
     }
 }
---
+++ KnifeAndSpoon/KnifeAndSpoon/Model/Ricetta.cs
@@ -0,0 +1,8 @@
+﻿namespace KnifeAndSpoon.Model
+{
+    public class Ricetta
+    {
+        public string Titolo { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
